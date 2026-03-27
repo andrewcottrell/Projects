0548243c91daabc49b88acf1790dc0eeca58134d
Andrew Cottrell <26989395@sun.ac.za>
2026-03-27 12:47:07 +0200

    Another roatation and some docs

--- CubeSolver/cube.py
+++ CubeSolver/cube.py
@@ -7,6 +7,20 @@ import copy
 
 ### 2D array is Row - Column
 class Cube:
+    """
+    A class that represents a 3x3 rubiks cube and supports rotation of each of the faces in both directions with default being clockwise.
+    Method is present for creating a fully solved cube.
+    TODO : Complete roatation functions for
+        top face
+        bottom face
+        right face
+        left face
+    TODO : Generate Random Cube - generate solved and move faces randomly?
+        concern - could be slow
+        might not be though.
+    TODO : Check cube validity on random generation maybe?
+
+    """
     def __init__(self):
         self._rows, self._cols = 3, 3
         self._topFace = [[0 for _ in range(self._cols)] for _ in range(self._rows)]
@@ -17,26 +31,97 @@ class Cube:
         self._bottomFace = [[0 for _ in range(self._cols)] for _ in range(self._rows)]
     
     #default is clockwise
-    def rotateFrontFace(self):
-        temp = copy.deepcopy(self._rightFace)
-        self._rightFace[0][0] = self._topFace[2][0]
-        self._rightFace[1][0] = self._topFace[2][1]
-        self._rightFace[2][0] = self._topFace[2][2]
+    def rotateFrontFace(self, default):
+        if (default):    
+            temp = copy.deepcopy(self._rightFace)
+            self._rightFace[0][0] = self._topFace[2][0]
+            self._rightFace[1][0] = self._topFace[2][1]
+            self._rightFace[2][0] = self._topFace[2][2]
 
-        self._topFace[2][0] = self._leftFace[0][2]
-        self._topFace[2][1] = self._leftFace[1][2]
-        self._topFace[2][2] = self._leftFace[2][2]
+            self._topFace[2][0] = self._leftFace[0][2]
+            self._topFace[2][1] = self._leftFace[1][2]
+            self._topFace[2][2] = self._leftFace[2][2]
 
-        self._leftFace[0][2] = self._bottomFace[0][0]
-        self._leftFace[1][2] = self._bottomFace[0][1]
-        self._leftFace[2][2] = self._bottomFace[0][2]
+            self._leftFace[0][2] = self._bottomFace[0][0]
+            self._leftFace[1][2] = self._bottomFace[0][1]
+            self._leftFace[2][2] = self._bottomFace[0][2]
 
-        self._bottomFace[0][0] = temp[0][0]
-        self._bottomFace[0][1] = temp[1][0]
-        self._bottomFace[0][2] = temp[2][0]
-        
-        
+            self._bottomFace[0][0] = temp[0][0]
+            self._bottomFace[0][1] = temp[1][0]
+            self._bottomFace[0][2] = temp[2][0]
+        else:
+            temp = copy.deepcopy(self._leftFace)
+            self._leftFace[0][2] = self._topFace[2][0]
+            self._leftFace[1][2] = self._topFace[2][1]
+            self._leftFace[2][2] = self._topFace[2][2]
 
+            self._topFace[2][0] = self._rightFace[0][0]
+            self._topFace[2][1] = self._rightFace[1][0]
+            self._topFace[2][2] = self._rightFace[2][0]
+
+            self._rightFace[0][0] = self._bottomFace[0][0]
+            self._rightFace[1][0] = self._bottomFace[0][1]
+            self._rightFace[2][0] = self._bottomFace[0][2]
+
+            self._bottomFace[0][0] = temp[0][2]
+            self._bottomFace[0][1] = temp[1][2]
+            self._bottomFace[0][2] = temp[2][2]
+    
+    def rotateBackFace(self, default):
+        if (default):    
+            temp = copy.deepcopy(self._rightFace)
+            self._rightFace[0][2] = self._topFace[0][0]
+            self._rightFace[1][2] = self._topFace[0][1]
+            self._rightFace[2][2] = self._topFace[0][2]
+
+            self._topFace[0][0] = self._leftFace[0][0]
+            self._topFace[0][1] = self._leftFace[1][0]
+            self._topFace[0][2] = self._leftFace[2][0]
+
+            self._leftFace[0][0] = self._bottomFace[2][0]
+            self._leftFace[1][0] = self._bottomFace[2][1]
+            self._leftFace[2][0] = self._bottomFace[2][2]
+
+            self._bottomFace[2][0] = temp[0][2]
+            self._bottomFace[2][1] = temp[1][2]
+            self._bottomFace[2][2] = temp[2][2]
+        else:
+            temp = copy.deepcopy(self._leftFace)
+            self._leftFace[0][0] = self._topFace[0][0]
+            self._leftFace[1][0] = self._topFace[0][1]
+            self._leftFace[2][0] = self._topFace[0][2]
+
+            self._topFace[0][0] = self._rightFace[0][2]
+            self._topFace[0][1] = self._rightFace[1][2]
+            self._topFace[0][2] = self._rightFace[2][2]
+
+            self._rightFace[0][2] = self._bottomFace[2][0]
+            self._rightFace[1][2] = self._bottomFace[2][1]
+            self._rightFace[2][2] = self._bottomFace[2][2]
+
+            self._bottomFace[2][0] = temp[0][0]
+            self._bottomFace[2][1] = temp[1][0]
+            self._bottomFace[2][2] = temp[2][0]
+    def rotateTopFace(self, default):
+        if (default):    
+            pass
+        else:
+            pass
+    def rotateBottomFace(self, default):
+        if (default):    
+            pass
+        else:
+            pass
+    def rotateLeftFace(self, default):
+        if (default):
+            pass
+        else:
+            pass
+    def rotateRightFace(self, default):
+        if (default):
+            pass
+        else:
+            pass
 
     def createSolvedCube(self):
         for row in range (self._rows):
@@ -58,6 +143,7 @@ class Cube:
             for col in range(self._cols):
                 self._bottomFace[row][col] = "Y"
 
+
     def displayCube(self):
         self._display_topFace()
         print()
@@ -70,24 +156,37 @@ class Cube:
         self._display_backFace()
         print()
         self._display_bottomFace()
+        print()
 
 
     def _display_frontFace(self):
+        print("Front:")
         for a in range(3):
             print(self._frontFace[a])
+
     def _display_topFace(self):
+        print("Top:")
         for a in range(3):
             print(self._topFace[a])
+
     def _display_leftFace(self):
+        print("Left:")
         for a in range(3):
             print(self._leftFace[a])
+
     def _display_rightFace(self):
+        print("Right:")
         for a in range(3):
             print(self._rightFace[a])
+
     def _display_bottomFace(self):
+        print("Bottom:")
         for a in range(3):
             print(self._bottomFace[a])
+
     def _display_backFace(self):
+        print("Back:")
         for a in range(3):
             print(self._backFace[a])
+
         
--- CubeSolver/main.py
+++ CubeSolver/main.py
@@ -1,10 +1,7 @@
 from cube import Cube
 
 cube1 = Cube()
-
 cube1.createSolvedCube()
-cube1.rotateFrontFace()
-cube1.displayCube()
-cube1.rotateFrontFace()
+cube1.rotateBackFace(False)
 cube1.displayCube()
 
